5249113e156fd32e3c320d1d49a9ba65227ff176
mdrazu2543 <alif.pab120na.12@gmail.com>
2022-10-24 12:35:34 +0600

    first commit

---
+++ resources/views/components/header.blade.php
@@ -0,0 +1,50 @@
+<nav class="navbar p-0  bg-light border">
+
+    <div class="container">
+        <div class=" w-100  d-flex py-2 fs-6 justify-content-between text-gray-600 ">
+            <div><i class="fa-solid fa-phone">
+                </i><a> +12-345-7890</a></div>
+            <div><i class="fa-solid fa-envelope"></i> <a> support@dummy.com </a>
+
+            </div>
+            <div><i class="fa-solid fa-building"></i> <a> XYZ, NY 01240, USA </a>
+
+            </div>
+
+        </div>
+
+
+
+        <div class="row w-100 justify-content-between  ">
+            <div class="col-12 col-md-2">
+                <img src="images/something.png" alt="something" />
+            </div>
+            <form class="d-flex col-12 col-md-7 align-items-center justify-content-center" role="search">
+                <input class="form-control input-lg rounded-pill px-4  " type="search" placeholder="Search..."
+                    aria-label="Search" style="max-width: 400px">
+                <button class="btn btn-outline-primary  rounded-pill mx-2 ">Search</button>
+            </form>
+            <div class="col-12 col-md-3 d-flex justify-content-around  align-items-center ">
+                <i class="fa-regular fa-heart fs-4 text-primary"></i>
+                <div class="position-relative btn">
+                    <span class="badge cur bg-warning position-absolute top-0  rounded-pill "
+                        style="transform: translateX(50%) translateY(-50%)">14</span>
+                    <i class="fa-solid fa-cart-shopping fs-5 text-primary "></i>
+                </div>
+                <a class="btn btn-primary">Login / Signup</a>
+
+            </div>
+        </div>
+        <div class="w-100 px-5 d-flex py-2 ">
+            <ul class="d-flex gap-4 w-100 cursor-pointer list-unstyled" >
+                <li class="btn btn-link text-white bg-primary text-decoration-none"><a>Home</a></li>
+                <li class="btn "><a>Products</a></li>
+                <li class="btn "><a>Blogs</a></li>
+                <li class="btn "><a>Contact</a></li>
+
+            </ul>
+        </div>
+
+    </div>
+
+</nav>
---
+++ resources/views/components/hero.blade.php
@@ -0,0 +1,138 @@
+<div class="mb-2">
+    <div class="row justify-content-around">
+        <div class="accordion col-3" id="accordionExample">
+            <div class="accordion-item">
+                <h2 class="accordion-header" id="headingOne">
+                    <button class="bg-primary text-white w-100 d-flex justify-content-between fs-4 py-2 px-3 border-0 "
+                        type="button" data-bs-toggle="collapse" data-bs-target="#collapseOne" aria-expanded="true"
+                        aria-controls="collapseOne">
+                        Categories
+                        <i class="fa-solid fa-bars"></i>
+                    </button>
+                </h2>
+                <div id="collapseOne" class="accordion-collapse collapse show" aria-labelledby="headingOne"
+                    data-bs-parent="#accordionExample">
+                    <div class="p-0 d-flex align-items-start justify-content-start border border-primary">
+                        <ul
+                            style="display:  flex; flex-direction: column; row-gap: 10px;  padding: 4px; padding-top: 6px; color : rgb(7, 22, 41); align-items: flex-start">
+                            <li class="btn text-primary fw-bold ">
+                                <i class="fa-solid fa-arrow-right"></i> Sports & Outdoors
+                            </li>
+                            <li class="btn">
+                                <i class="fa-solid fa-arrow-right"></i> Electronics
+                            </li>
+
+                            <li class="btn">
+                                <i class="fa-solid fa-arrow-right"></i> Automotive & Motocycle
+                            </li>
+                            <li class="btn">
+                                <i class="fa-solid fa-arrow-right"></i> Health & Beauty
+                            </li>
+                            <li class="btn">
+                                <i class="fa-solid fa-arrow-right"></i> Smartphone & Tablets
+                            </li>
+                            <li class="btn">
+                                <i class="fa-solid fa-arrow-right"></i> Flashlights & Lamps
+                            </li>
+
+                        </ul>
+                    </div>
+                </div>
+            </div>
+
+        </div>
+
+        <div class="col-7  p-0">
+            <!-- Slider main container -->
+            <div class="swiper  w-100" style="height: 390px">
+                <!-- Additional required wrapper -->
+                <div class="swiper-wrapper  w-100">
+                    <!-- Slides -->
+                    <div class="swiper-slide overflow-hidden position-relative">
+                        <img style="object-fit: fill "
+                            src="https://www.themagnifico.net/eard/ecommerce-wordpress-theme/wp-content/themes/ecommerce-zone-pro/assets/images/offer1/banner1.jpg"
+                            alt="eventimage" />
+                        <div class="content position-absolute" style="top: 80px; left : 57px">
+                            <h1 class="text-primary fw-bolder">99</h1>
+                            <h2 class="fw-bolder">New Arrivals!!!</h2>
+                            <p>Checkout the summer collections</p>
+
+                        </div>
+
+                    </div>
+                    <div class="swiper-slide overflow-hidden position-relative">
+                        <img style="object-fit: fill "
+                            src="https://www.themagnifico.net/eard/ecommerce-wordpress-theme/wp-content/themes/ecommerce-zone-pro/assets/images/offer1/banner1.jpg"
+                            alt="eventimage" />
+                        <div class="content position-absolute" style="top: 80px; left : 57px">
+                            <h1 class="text-primary fw-bolder">100+</h1>
+                            <h2 class="fw-bolder">New Arrivals!!!</h2>
+                            <p>Checkout the winter collections</p>
+
+                        </div>
+
+                    </div>
+                    <div class="swiper-slide overflow-hidden position-relative">
+                        <img style="object-fit: fill "
+                            src="https://www.themagnifico.net/eard/ecommerce-wordpress-theme/wp-content/themes/ecommerce-zone-pro/assets/images/offer1/banner1.jpg"
+                            alt="eventimage" />
+                        <div class="content position-absolute" style="top: 80px; left : 57px">
+                            <h1 class="text-primary fw-bolder">50</h1>
+                            <h2 class="fw-bolder">New Arrivals!!!</h2>
+                            <p>Checkout the fall collections</p>
+
+                        </div>
+
+                    </div>
+
+
+                </div>
+
+            </div>
+
+            <div class="row mb-4  justify-content-between">
+                <div class="col-5 overflow-hidden  gap-6 position-relative">
+                    <img height="150" width="320" style="object-fit: contain border border-primary"
+                        src="https://www.themagnifico.net/eard/ecommerce-wordpress-theme/wp-content/themes/ecommerce-zone-pro/assets/images/offer1/banner2.jpg"
+                        alt="something" />
+
+                    <div class="position-absolute fw-bold" style="top: 50px; right: 0px">
+                        New Watch Collection
+                        <div class="mx-5"> <button class="btn btn-outline-primary">Buy Now</button>
+                        </div>
+                    </div>
+
+                </div>
+
+                <div class="col-6 overflow-hidden gap-6 position-relative">
+                    <img height="150" width="320" style="object-fit: contain border border-primary"
+                        src="https://www.themagnifico.net/eard/ecommerce-wordpress-theme/wp-content/themes/ecommerce-zone-pro/assets/images/offer1/banner2.jpg"
+                        alt="something" />
+
+                    <div class="position-absolute fw-bold" style="top: 50px; right: 60px">
+                        New Watch Collection
+                        <div class="mx-5"> <button class="btn btn-outline-primary">Buy Now</button>
+                        </div>
+                    </div>
+
+                </div>
+
+            </div>
+
+        </div>
+
+        <div class="col-2 overflow-hidden position-relative">
+            <!-- Slider main container -->
+            <img src="https://www.themagnifico.net/eard/ecommerce-wordpress-theme/wp-content/themes/ecommerce-zone-pro/assets/images/offer1/arrival_bg.jpg"
+                alt="something" />
+
+            <div class="position-absolute fw-bold" style="top: 50px; right: 0px">
+                New Dress Collection
+                <div class="mx-5"> <button class="btn btn-outline-primary">Buy Now</button>
+                </div>
+            </div>
+        </div>
+    </div>
+
+</div>
+</div>
